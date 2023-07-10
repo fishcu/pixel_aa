30d5e7fbf57f8535ba237cddb11af0cc3babffba
fishcu <isaacbd@gmail.com>
2023-07-10 23:38:53 +0200

    Fix MM build?

--- tcc_arm_build.sh
+++ tcc_arm_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-cd pixel_aa/deps/tinycc/
+cd ./deps/tinycc/
 ./configure --cpu=arm --extra-cflags="-marm -march=armv7ve+simd -mtune=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard" --cross-prefix=$CROSS_COMPILE --sysroot=/opt/miyoomini-toolchain/arm-linux-gnueabihf/libc
 ln -s /opt/miyoomini-toolchain/arm-linux-gnueabihf/libc/lib/* /lib/
 make arm-libtcc1-usegcc=yes
+make arm-libtcc1-usegcc=yes cross-arm
