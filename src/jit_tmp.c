d81d9283c970e132034ccf2ed9669b811a1087df
fishcu <isaacbd@gmail.com>
2023-07-17 23:23:26 +0200

    Using pointers is a bit faster on the MM instead of offsets

--- src/jit_tmp.c
+++ src/jit_tmp.c
@@ -1,51 +1,54 @@
 #include <stdint.h>
-static inline void fill_row(const uint32_t* restrict in, uint32_t* restrict out,
-                            int out_y_offset, int in_y_offset) {
-    for (int x = 0, in_x = 0; x < 640; x += 5, in_x += 2) {
-        out[out_y_offset + x] = out[out_y_offset + x + 1] =
-            out[out_y_offset + x + 2] = in[in_y_offset + in_x + 0];
-        out[out_y_offset + x + 3] = out[out_y_offset + x + 4] =
-            in[in_y_offset + in_x + 1];
+static inline void fill_row(const uint32_t* restrict in,
+                            uint32_t* restrict out) {
+    for (int x = 128; x > 0; --x) {
+        *out++ = *in;
+        *out++ = *in;
+        *out++ = *in++;
+        *out++ = *in;
+        *out++ = *in++;
     }
 }
-void kernel(const uint32_t* restrict in, uint32_t* restrict out) {
-    for (int y = 0, out_y_offset = 0, in_y_offset = 0; y < 32; ++y) {
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        in_y_offset += 256;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        in_y_offset += 256;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        in_y_offset += 256;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        in_y_offset += 256;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        in_y_offset += 256;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        in_y_offset += 256;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        fill_row(in, out, out_y_offset, in_y_offset);
-        out_y_offset += 640;
-        in_y_offset += 256;
+void kernel(const uint32_t* restrict in_ptr, uint32_t* restrict out_ptr) {
+    const uint32_t* restrict in = in_ptr;
+    uint32_t* restrict out = out_ptr;
+    for (int y = 32; y > 0; --y) {
+        fill_row(in, out);
+        out += 640;
+        fill_row(in, out);
+        out += 640;
+        fill_row(in, out);
+        out += 640;
+        in += 256;
+        fill_row(in, out);
+        out += 640;
+        fill_row(in, out);
+        out += 640;
+        in += 256;
+        fill_row(in, out);
+        out += 640;
+        fill_row(in, out);
+        out += 640;
+        in += 256;
+        fill_row(in, out);
+        out += 640;
+        fill_row(in, out);
+        out += 640;
+        in += 256;
+        fill_row(in, out);
+        out += 640;
+        fill_row(in, out);
+        out += 640;
+        in += 256;
+        fill_row(in, out);
+        out += 640;
+        fill_row(in, out);
+        out += 640;
+        in += 256;
+        fill_row(in, out);
+        out += 640;
+        fill_row(in, out);
+        out += 640;
+        in += 256;
     }
 }
