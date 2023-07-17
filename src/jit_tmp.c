7bc4dfa397fcb4f0c13b867c020a0c235d73b3ba
root <root@isaac-lap-win>
2023-07-17 23:32:41 +0200

    Switching to Macro instead of function makes 0 speed difference. Keep for documentation in a branch

--- src/jit_tmp.c
+++ src/jit_tmp.c
@@ -1,54 +1,38 @@
 #include <stdint.h>
-static inline void fill_row(const uint32_t* restrict in,
-                            uint32_t* restrict out) {
-    for (int x = 128; x > 0; --x) {
-        *out++ = *in;
-        *out++ = *in;
-        *out++ = *in++;
-        *out++ = *in;
-        *out++ = *in++;
+#define fill_row(in, out)           \
+    for (int x = 128; x > 0; --x) { \
+        *(out)++ = *(in);           \
+        *(out)++ = *(in);           \
+        *(out)++ = *(in)++;         \
+        *(out)++ = *(in);           \
+        *(out)++ = *(in)++;         \
     }
-}
 void kernel(const uint32_t* restrict in_ptr, uint32_t* restrict out_ptr) {
     const uint32_t* restrict in = in_ptr;
     uint32_t* restrict out = out_ptr;
     for (int y = 32; y > 0; --y) {
         fill_row(in, out);
-        out += 640;
+        in -= 256;
         fill_row(in, out);
-        out += 640;
+        in -= 256;
         fill_row(in, out);
-        out += 640;
-        in += 256;
         fill_row(in, out);
-        out += 640;
+        in -= 256;
         fill_row(in, out);
-        out += 640;
-        in += 256;
         fill_row(in, out);
-        out += 640;
+        in -= 256;
         fill_row(in, out);
-        out += 640;
-        in += 256;
         fill_row(in, out);
-        out += 640;
+        in -= 256;
         fill_row(in, out);
-        out += 640;
-        in += 256;
         fill_row(in, out);
-        out += 640;
+        in -= 256;
         fill_row(in, out);
-        out += 640;
-        in += 256;
         fill_row(in, out);
-        out += 640;
+        in -= 256;
         fill_row(in, out);
-        out += 640;
-        in += 256;
         fill_row(in, out);
-        out += 640;
+        in -= 256;
         fill_row(in, out);
-        out += 640;
-        in += 256;
     }
 }
