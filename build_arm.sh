4cfd220e42ac02c6581d6b8b546bf33c17fdc130
fishcu <isaacbd@gmail.com>
2023-07-24 18:45:54 +0200

    Move to clang build; Disable TCC and llvm demos for now

--- build_arm.sh
+++ build_arm.sh
@@ -2,5 +2,5 @@
 set -e
 
 mkdir -p _build_arm
-cmake . -DBUILD_FOR_MM=ON -B _build_arm
+cmake . -DCMAKE_TOOLCHAIN_FILE=/home/isaac/mm_toolchain.cmake -DBUILD_FOR_MM=ON -B _build_arm
 VERBOSE=1 cmake --build _build_arm
